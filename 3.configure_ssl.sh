c4304ec3c77fc095321440bd35546c2c01eef5bf
kzsoe <kzsoe@ipcmyanmar.com>
2022-07-26 21:38:42 +0630

    fixed_initial_db

--- 3.configure_ssl.sh
+++ 3.configure_ssl.sh
@@ -6,14 +6,13 @@ echo "Please setup above IP address as A/AAA DNS entry in domain setting before 
 while [ true ] ; do
 read -t 3 -n 1
 if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting.."
-fi
-done
-
 echo "setting up SSL.."
 sudo certbot --apache --agree-tos --redirect -m ${contact} -d ${domain}
 
 echo "Reloading Web server..."
 sudo systemctl restart apache2
+exit ;
+else
+echo "waiting.."
+fi
+done
